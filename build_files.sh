b58a87fe078b1902b65eb8c65099c787e7ba0cb1
Jado0o0o <76520626+ailaa-jadoo@users.noreply.github.com>
2024-01-02 20:05:20 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 echo "BUILD START"
- python -m pip install -r requirements.txt
- python manage.py runserver
+ python3.9 -m pip install -r requirements.txt
+ python3.9 manage.py runserver
  echo "BUILD END"
